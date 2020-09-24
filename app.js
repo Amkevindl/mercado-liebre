2c738a70b2508b82a5f949266b529c3d6140621b
amkevindl <amkevindl@gmail.com>
2020-09-24 20:46:41 -0300

    Modificando estructura html

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get('/',function(req,res){
     let file=path.resolve('./index.html');
     res.sendFile(file)
 })
+app.get('/register',function(req,res){
+    let file=path.resolve('public/register.html');
+    res.sendFile(file)
+})
+app.get('/login',function(req,res){
+    let file=path.resolve('public/login.html');
+    res.sendFile(file)
+})
 app.get('*',function(req,res){
     if(req.url.includes('.')){
         let file=path.resolve('public'+req.url)
